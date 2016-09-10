23037ca5c252862879b7c11067dd410ee55dbc30
Francisco Mendoza <jfranciscomn>
2016-09-10 12:15:35 -0600

    Theme

---
+++ movil/client/scripts/controllers/dash.controller.js
@@ -0,0 +1,13 @@
+
+angular
+  .module('FLOKsports')
+  .controller('DashCtrl', DashCtrl);
+ 
+function DashCtrl($scope, $reactive, $state, $ionicLoading, $ionicPopup, $log, $ionicModal) {
+  $reactive(this).attach($scope);
+   
+  	
+  
+
+ 
+}
