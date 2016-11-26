7e6dfaaac6183841ddfdc01b507e82d03991c2c9
Francisco Mendoza <jfranciscomn@gmail.com>
2016-11-26 13:33:45 -0700

    -

--- movil/mobile-config.js
+++ movil/mobile-config.js
@@ -17,29 +17,52 @@ App.setPreference('BackgroundColor', '0x0023adc2');
 
 
 
+
+
 App.icons({
   // iOS
-  'iphone': 'resources/icons/icon-60x60.png',
-  'iphone_2x': 'resources/icons/icon-60x60@2x.png',
-  'iphone_3x': 'resources/icons/icon-60x60@3x.png',
+  //'ios_settings': 'resources/icons/icon-29x29.png',
+  //'ios_settings_2x': 'resources/icons/icon-58x58.png',
+  //'ios_settings_3x': 'resources/icons/icon-87x87.png',
+
+  //'ios_spotlight': 'resources/icons/icon-40x40.png',
+  //'ios_spotlight_2x': 'resources/icons/icon-80x80.png',
+
+  //iPhone
+  'iphone': 'resources/icons/icon-120x120.png',
+  'iphone_2x': 'resources/icons/icon-120x120.png',
+  'iphone_3x': 'resources/icons/icon-180x180.png',
+
+  //iPad
+  'ipad': 'resources/icons/icon-76x76.png',
+  'ipad_2x': 'resources/icons/icon-152x152.png',
+
 
   // Android
-  'android_ldpi': 'resources/icons/icon-36x36.png',
+  'android_ldpi': 'resources/icons/icon-48x48.png',
   'android_mdpi': 'resources/icons/icon-48x48.png',
   'android_hdpi': 'resources/icons/icon-72x72.png',
   'android_xhdpi': 'resources/icons/icon-96x96.png'
 });
 
+
+
+
 App.launchScreens({
-  // iOS
+  // iPhone
 	'iphone': 'resources/splash/splash-320x480.png',
-  'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-  'iphone5': 'resources/splash/splash-320x568@2x.png',
-  'iphone6': 'resources/splash/splash-375x667@2x.png',
-  'iphone6p_portrait': 'resources/splash/splash-414x736@3x.png',
+  'iphone_2x': 'resources/splash/splash-640x960.png',
+  'iphone5': 'resources/splash/splash-640x1136.png',
+  'iphone6': 'resources/splash/splash-750x1334.png',
+  'iphone6p_portrait': 'resources/splash/splash-1242x2208.png',
+
+  // iPad
+  'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  'ipad_portrait_2x': 'resources/splash/splash-1536x2048.png',
+
   // Android
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png'
+  'android_ldpi_portrait': 'resources/splash/splash-320x470.png',
+  'android_mdpi_portrait': 'resources/splash/splash-320x470.png',
+  'android_hdpi_portrait': 'resources/splash/splash-480x640.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-720x960.png'
 });
